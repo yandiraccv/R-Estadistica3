24d39fb64cb84a155d14f4c5c4ec26f32923c778
RUsersGroup-Ecuador <33700488+RUsersGroup-Ecuador@users.noreply.github.com>
2017-11-19 16:58:14 -0500

    Add files via upload

---
+++ shinydash v local.R
@@ -0,0 +1,62 @@
+## app.R ##
+library(shiny)
+library(shinydashboard)
+#library(flexdashboard)
+library(shiny)
+library(dplyr)
+# library(DBI)
+# library(RPostgreSQL)
+load("C:/Users/crbdl/Desktop/datos.RData")
+
+ui <- dashboardPage(
+  dashboardHeader(title = "Consulta postgres"),
+  dashboardSidebar(
+    radioButtons("tabla", "Seleccionar tabla", 
+                 choices = c("CIAS"#, "ACCIONISTAS", "REPRESENTANTES"
+                             ),
+                 selected = "CIAS"),
+    numericInput("n", "Numero de registros", value = 2,min = 1, max = 220092),
+    textInput("ruc", "RUC")
+  ),
+  dashboardBody(
+    
+    textOutput("n"),
+    dataTableOutput("tabla")
+    
+  )
+)
+
+server <- function(input, output) { 
+  
+  n <- reactive(input$n)
+  
+  output$n <- renderText(paste("Numero de registros:", n()))
+  
+
+  data <- reactive({
+    # db <- dbConnect(drv=dbDriver("PostgreSQL"), 
+    #                 dbname = "SC",
+    #                 host = "localhost",port = 5432, 
+    #                 user = "postgres",
+    #                 password = "sintiempo")
+    # # nombre_tabla <- if(input$tabla=='CIAS') {"companias_15092016"} else{
+    # #   "accionistas_15092016"
+    # # }
+    # nombre_tabla <-switch(input$tabla,
+                          # "CIAS" = "companias_15092016",
+                          # "ACCIONISTAS" = "accionistas_15092016",
+                          # "REPRESENTANTES" = "representantes_15092016")
+                          # 
+    # query <- paste("SELECT * FROM", nombre_tabla, " LIMIT ", n())
+    # data <- dbGetQuery(db, query)
+    # dbDisconnect(db)
+  data <- cias %>% filter(RUC==input$ruc)
+  
+  data})
+  
+  output$tabla <- renderDataTable(data())
+  
+  }
+
+shinyApp(ui, server)
+
