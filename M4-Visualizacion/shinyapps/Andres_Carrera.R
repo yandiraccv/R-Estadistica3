810088ba69156c7b9e431535c26d36409ad01c56
RUsersGroup-Ecuador <33700488+RUsersGroup-Ecuador@users.noreply.github.com>
2017-11-28 12:50:57 -0500

    https://andysebapps.shinyapps.io/dashboard/

--- M4-Visualizacion/shinyapps/Andres_Carrera.R
+++ M4-Visualizacion/shinyapps/Andres_Carrera.R
@@ -18,7 +18,7 @@ library(scales)
 library(plotly)
 library(data.table)
 library(DT)
-
+ 
 #meses<-c("enero","febrero","marzo","abril","mayo","junio","julio","agosto","septiembre","octubre","noviembre","diciembre")
 meses<-c("January","February","March","April","May","June","July","August","September","October","November","December")
 n_dias<-c(31,28,31,30,31,30,31,31,30,31,30,31)
